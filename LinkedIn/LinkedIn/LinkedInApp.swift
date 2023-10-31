50cc7e21e13c72c2e9275e53437c1f36644bd3d2
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-31 10:47:19 +0530

    initial commit

---
+++ LinkedIn/LinkedIn/LinkedInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInApp.swift
+//  LinkedIn
+//
+//  Created by Gurjinder Singh on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
